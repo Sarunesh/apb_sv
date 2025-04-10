216390d4a30a76345c8d3e6087141a42f1a5e256
Sarunesh <sarunesh.s12@gmail.com>
2025-04-10 12:34:48 +0530

    Combined 2 drive tasks into 1 in apb_bfm file

--- apb_bfm.sv
+++ apb_bfm.sv
@@ -17,18 +17,15 @@ class apb_bfm;
 		forever begin
 			tx=new();
 			gen2bfm.get(tx);
-			if(apb_common::testcase_wait==1)
-				drive_wait(tx);
-			else
-				drive_no_wait(tx);
+			drive(tx);
 			tx.print("BFM");
 			apb_common::bfm_count++;
 		end
 	endtask
 
-	// drive tasks
-	task drive_no_wait(ref apb_tx tx);
-		$display("#### Inside drive_no_wait task in bfm");
+	// drive task
+	task drive(apb_tx tx);
+		$display("#### Inside drive task in bfm");
 		@(posedge vif.pclk);
 		vif.trans_i	<= tx.trans_i;			// SP
 		vif.addr_i	<= tx.addr_i;			// SP
@@ -36,6 +33,18 @@ class apb_bfm;
 		vif.wdata_i	<= tx.wdata_i;			// SP
 		vif.pready	<= tx.pready;
 		@(posedge vif.pclk);
+		if(apb_common::testcase_wait==1)begin
+			fork
+				begin: PREADY_ASSERT				// Process-1
+					repeat(2)@(posedge vif.pclk);	// Maintains pready=0 for 2 clock cycles
+					tx.pready = 1'b1;				// To exit from the wait state
+				end
+				begin: PREADY_WAIT					// Process-2
+					wait(tx.pready==1);
+					vif.pready	= tx.pready;
+				end
+			join
+		end
 		tx.pselx	= vif.pselx;			// AP; towards peripheral
 		tx.penable	= vif.penable;			// AP; towards peripheral
 		tx.pwrite	= vif.pwrite;			// AP; towards peripheral
@@ -43,33 +52,5 @@ class apb_bfm;
 		tx.pwdata	= vif.pwdata;			// AP; towards peripheral
 		tx.trans_err_o = vif.trans_err_o;	// AP; towards peripheral
 		tx.rdata_o	= vif.rdata_o;
-	endtask
-
-	task drive_wait(ref apb_tx tx);
-		$display("#### Inside drive_wait task in bfm");
-		@(posedge vif.pclk);
-		vif.trans_i	<= tx.trans_i;			// SP
-		vif.addr_i	<= tx.addr_i;			// SP
-		vif.wr_rd_i	<= tx.wr_rd_i;			// SP
-		vif.wdata_i	<= tx.wdata_i;			// SP
-		vif.pready	<= tx.pready;
-		@(posedge vif.pclk);
-		fork
-			begin: PREADY_ASSERT					// Process-1
-				repeat(2)@(posedge vif.pclk);		// Maintains pready=0 for 2 clock cycles
-				tx.pready = 1'b1;					// To exit from the wait state
-			end
-			begin: PREADY_WAIT						// Process-2
-				wait(tx.pready==1);
-				vif.pready	= tx.pready;
-				tx.pselx	= vif.pselx;			// AP; towards peripheral
-				tx.penable	= vif.penable;			// AP; towards peripheral
-				tx.pwrite	= vif.pwrite;			// AP; towards peripheral
-				tx.paddr	= vif.paddr;			// AP; towards peripheral
-				tx.pwdata	= vif.pwdata;			// AP; towards peripheral
-				tx.trans_err_o = vif.trans_err_o;	// AP; towards peripheral
-				tx.rdata_o	= vif.rdata_o;
-			end
-		join
 	endtask
 endclass

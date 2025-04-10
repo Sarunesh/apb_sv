579df427d1baa4122e3b0bf4132279b1d7123ac1
Sarunesh <sarunesh.s12@gmail.com>
2025-04-10 10:07:21 +0530

    Added a testcase with wait scenario.
      Included logic for wait scenario in apb_gen.sv and apb_bfm.sv

--- apb_bfm.sv
+++ apb_bfm.sv
@@ -44,5 +44,29 @@ class apb_bfm;
 
 	task drive_wait(ref apb_tx tx);
 		$display("Inside drive_wait task in bfm");
+		@(posedge vif.pclk);
+		vif.trans_i	<= tx.trans_i;			// SP
+		vif.addr_i	<= tx.addr_i;			// SP
+		vif.wr_rd_i	<= tx.wr_rd_i;			// SP
+		vif.wdata_i	<= tx.wdata_i;			// SP
+		vif.pready	<= tx.pready;
+		@(posedge vif.pclk);
+		fork
+			begin: PREADY_ASSERT					// Process-1
+				repeat(2)@(posedge vif.pclk);		// Maintains pready=0 for 2 clock cycles
+				tx.pready = 1'b1;					// To exit from the wait state
+			end
+			begin: PREADY_WAIT						// Process-2
+				wait(tx.pready==1);
+				vif.pready	= tx.pready;
+				tx.pselx	= vif.pselx;			// AP; towards peripheral
+				tx.penable	= vif.penable;			// AP; towards peripheral
+				tx.pwrite	= vif.pwrite;			// AP; towards peripheral
+				tx.paddr	= vif.paddr;			// AP; towards peripheral
+				tx.pwdata	= vif.pwdata;			// AP; towards peripheral
+				tx.trans_err_o = vif.trans_err_o;	// AP; towards peripheral
+				tx.rdata_o	= vif.rdata_o;
+			end
+		join
 	endtask
 endclass
--- apb_gen.sv
+++ apb_gen.sv
@@ -9,11 +9,21 @@ class apb_gen;
 	task run();
 		$display("### Inside run task of generator");
 		case(apb_common::testcase)
+		// Scenario-1
 			"write_no_wait":begin
 				tx=new();
 				if(!tx.randomize() with {tx.trans_i==1;tx.wr_rd_i==1;tx.pready==1;})
-					$display("Randomization failed at generator at %0t", $time);
+					$display("%0s: Randomization failed at generator at %0t", apb_common::testcase, $time);
 				apb_common::testcase_wait=0;
+				gen2bfm.put(tx);
+				tx.print("GENERATOR");
+			end
+		// Scenario-2
+			"write_wait":begin
+				tx=new();
+				if(!tx.randomize() with {tx.trans_i==1;tx.wr_rd_i==1;tx.pready==0;})
+					$display("%0s: Randomization failed at generator at %0t", apb_common::testcase, $time);
+				apb_common::testcase_wait=1;
 				gen2bfm.put(tx);
 				tx.print("GENERATOR");
 			end
